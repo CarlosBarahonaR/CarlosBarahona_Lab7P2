e791e75872dae11a69d97dbed2453ef4268716da
CJBarahona <cbarahona@t-developing.com>
2022-06-03 15:46:51 -0600

    Diseño de artista

--- CarlosBarahona_Lab7P2/src/carlosbarahona_lab7p2/SpotifyApp.java
+++ CarlosBarahona_Lab7P2/src/carlosbarahona_lab7p2/SpotifyApp.java
@@ -417,7 +417,7 @@ public class SpotifyApp extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        // TODO add your handling code here:
+        // Inicio de sesión de usuario
         String usuario = usuarioTextField.getText();
         String contrasena = new String(contraseñaTextField.getPassword());
 
